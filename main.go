31c137d00c42c738f7bf9d87f817732a8bd3a6ca
Jean Lucas Lima <jeanleonino@gmail.com>
2021-06-09 17:28:03 -0300

    Hello World

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	b, err := tb.NewBot(tb.Settings{
+		// You can also set custom API URL.
+		// If field is empty it equals to "https://api.telegram.org".
+		URL: "https://api.telegram.org",
+
+		Token:  "1861037011:AAHS_ofMQ_nOz92-HeXQSCKOKTrTten2hbk",
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Chat, "Hello World!")
+	})
+
+	b.Handle("/sobre", func(m *tb.Message) {
+		b.Send(m.Chat, "Bot open source em Go, visite: github.com/jeanlucaslima/tgbot/")
+	})
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Chat, "Hello World!")
+	})
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Chat, "Hello World!")
+	})
+
+	b.Start()
+}
